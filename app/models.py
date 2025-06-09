7a026a6ed146b9b6ac223514887d36acc9aa8905
Мазур Владислав Сергеевич <vladislav.mazur@softclub.by>
2025-06-09 14:07:38 +0300

    Task 4 complete

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
-    password_hash: so.Mapped[str] = so.mapped_column(sa.String(256))
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates="author")
 
